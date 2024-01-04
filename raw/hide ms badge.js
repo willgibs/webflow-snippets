4ee78786ca84e29d96765997cc0b1286ea7f8353
Will Gibson <hello@willgib.com>
2024-01-04 14:22:18 -0500

    raw code added

---
+++ raw/hide ms badge.js
@@ -0,0 +1,7 @@
+<!-- Remove Memberstack 'Test Mode' badge -->
+<style>
+  .ms-test-label {
+    visibility: hidden !important;
+    display: none !important;
+  }
+</style>
---
+++ raw/tab links.js
@@ -0,0 +1,39 @@
+<!-- Tab links for modal -->
+<script>
+  var Webflow = Webflow || [];
+  Webflow.push(() => {
+    function changeTab(shouldScroll = false) {
+      const tabName = window.location.hash.substring(1);
+      const tabTarget = document.querySelector(`[data-w-tab="${tabName}"]`);
+
+      if (tabTarget) {
+
+        tabTarget.click();
+
+        if (shouldScroll) {
+          const offset = 0; // Change this to match a fixed header height
+          window.scrollTo({
+            top: $(tabTarget).offset().top - offset, behavior: 'smooth'
+          });
+        }
+      }
+    }
+
+    const tabs = document.querySelectorAll('[data-w-tab]');
+
+    tabs.forEach(tab => {
+      const dataWTabValue = tab.dataset.wTab;
+      const pargedDataTab = dataWTabValue.replace(/\s+/g,"-").toLowerCase();
+      tab.dataset.wTab = pargedDataTab;
+      tab.addEventListener('click', () => {
+        history.pushState({}, '', `#${pargedDataTab}`);
+      });
+    });
+
+  	if (window.location.hash) {
+      requestAnimationFrame(() => { changeTab(true); });
+    }
+
+    window.addEventListener('hashchange', () => { changeTab() });
+  });
+</script>
